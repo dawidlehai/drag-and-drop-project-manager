849ac35add507ba62d53e8e532bae0a3484e4e23
dawidlehai <haidawidle@gmail.com>
2023-01-08 12:47:13 +0100

    Added visual styles on drag and drop

--- src/ProjectList.ts
+++ src/ProjectList.ts
@@ -1,12 +1,13 @@
-import { Project, ProjectStatus } from './type-definitions';
+import { DragTarget, Project, ProjectStatus } from './type-definitions';
 import projectState from './ProjectState';
 import Component from './Component';
 import ProjectItem from './ProjectItem';
+import { autobind } from './decorators';
 
-export default class ProjectList extends Component<
-  HTMLDivElement,
-  HTMLElement
-> {
+export default class ProjectList
+  extends Component<HTMLDivElement, HTMLElement>
+  implements DragTarget
+{
   assignedProjects: Project[];
 
   constructor(private type: 'active' | 'finished') {
@@ -15,6 +16,37 @@ export default class ProjectList extends Component<
 
     this.configure();
     this.renderContent();
+  }
+
+  @autobind
+  dragOverHandler(_: DragEvent): void {
+    const listEl = this.element.querySelector('ul')!;
+    listEl.classList.add('droppable');
+  }
+
+  dropHandler(_: DragEvent): void {}
+
+  @autobind
+  dragLeaveHandler(_: DragEvent): void {
+    const listEl = this.element.querySelector('ul')!;
+    listEl.classList.remove('droppable');
+  }
+
+  configure() {
+    this.element.addEventListener('dragover', this.dragOverHandler);
+    this.element.addEventListener('dragleave', this.dragLeaveHandler);
+    this.element.addEventListener('drop', this.dropHandler);
+
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
+      this.renderProjects();
+    });
   }
 
   private renderProjects() {
@@ -32,18 +64,5 @@ export default class ProjectList extends Component<
     this.element.querySelector('ul')!.id = listId;
     this.element.querySelector('h2')!.textContent =
       this.type.toUpperCase() + ' PROJECTS';
-  }
-
-  configure() {
-    projectState.addListener((projects: Project[]) => {
-      const relevantProjects = projects.filter((prj) => {
-        if (this.type === 'active') {
-          return prj.status === ProjectStatus.Active;
-        }
-        return prj.status === ProjectStatus.Finished;
-      });
-      this.assignedProjects = relevantProjects;
-      this.renderProjects();
-    });
   }
 }
